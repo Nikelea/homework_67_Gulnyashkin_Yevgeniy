5a75b5c769eef363da5989b414fd3dd6f8b9bb4c
Гульняшкин Евгений <ducat.ea@gmail.com>
2023-04-07 17:11:02 +0600

    Пагинация настроена и добавленна.

--- Задание 2/index.js
+++ Задание 2/index.js
@@ -1,39 +1,43 @@
 div_main = document.getElementById('container');
 
 const BASE_URL = 'https://rickandmortyapi.com/api'
-var number = 1
 
-let xhr = new XMLHttpRequest();
-xhr.open("GET", BASE_URL + "/character?page=" + number);
-xhr.send();
+console.log('test');
+function loadMainPage(someURL) {
 
-xhr.onload = function(){  
-  let data = JSON.parse(this.response);
-  for ( char of data.results) {
-    let div = document.createElement('div');
-    div.className = "card";
-    div.innerHTML = '<img src="'+ char.image +'" class="card-img-top" alt="...">' +
-        '<div class="card-body">' + 
-          '<h5 class="card-title">ФИО: ' + char.name + '</h5>' + 
-          '<p class="card-text"> Происхождение: ' + char.origin.name + '</p>' +
-          '<a href="char.html?id=' + char.id + '" class="btn btn-primary">Detail</a></div>';
-    div_main.append(div);
-  }
+    let xhr = new XMLHttpRequest();
 
+    xhr.open("GET", someURL);
+    xhr.send();
 
-  let button_next = document.createElement('a')
-  button_next.href = '#'
-  button_next.innerHTML = 'Next'
-  div_main.append(button_next)
-  button_next.addEventListener('click', function() { number = number + 1; console.log(number);   window.location.reload(); });
-  
-  // let button_previos = document.createElement('a')
-  // button_previos.href = '#'
-  // button_previos.innerHTML = 'Prev'
-  // div_main.append(button_previos)
-  // number -= 1
+    xhr.onload = function () {
+        let data = JSON.parse(this.response);
+        for (char of data.results) {
+            let div = document.createElement('div');
+            div.className = "card";
+            div.innerHTML = '<img src="' + char.image + '" class="card-img-top" alt="...">' +
+                '<div class="card-body">' +
+                '<h5 class="card-title">ФИО: ' + char.name + '</h5>' +
+                '<p class="card-text"> Происхождение: ' + char.origin.name + '</p>' +
+                '<a href="char.html?id=' + char.id + '" class="btn btn-primary">Detail</a></div>';
+            div_main.append(div);
+        }
 
-console.log(number);
-
-   // вывод данных в разметке и пр. полезные действия
+        if (data.info.next !== null) {
+            let button_next = document.createElement('button')
+            button_next.className = 'btn btn-primary next'
+            button_next.innerHTML = 'Next'
+            div_main.append(button_next)
+            button_next.addEventListener('click', function () { div_main.innerHTML = ''; loadMainPage(data.info.next) });
+        }
+        if (data.info.prev !== null) {
+            let button_prev = document.createElement('button')
+            button_prev.className = 'btn btn-primary prev'
+            button_prev.innerHTML = 'Prev'
+            div_main.append(button_prev)
+            button_prev.addEventListener('click', function () { div_main.innerHTML = ''; loadMainPage(data.info.prev) });
+        }
+    }
 };
+
+loadMainPage("https://rickandmortyapi.com/api/character")
